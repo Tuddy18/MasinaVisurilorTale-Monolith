a9275eb50f9017510c97e1cde4f65f4c4a2ad302
alexgavrilas <gavrilas.alex@gmail.com>
2020-03-21 18:40:32 +0200

    Rusu drifting Cover Pic + UI fixes

--- app/cart/cart.py
+++ app/cart/cart.py
@@ -4,20 +4,20 @@ from app.config import mysql
 from app.user.login import is_logged_in
 
 
-@app.route('/addToCart/<int:id>')
+@app.route('/addToCart/<path:name>')
 @is_logged_in
-def addToCart(id):
-    cur = mysql.connection.cursor()
-    result = cur.execute("SELECT * FROM cart where user_id = {} and state = 'ACTIVE'".format(session['userId']))
-    if result == 0:
-        cur.execute("INSERT INTO cart(user_id, total_price, state) VALUES ({}, 0, 'ACTIVE')".format(session['userId']))
-    cur.execute("SELECT * FROM cart where user_id = {} and state = 'ACTIVE'".format(session['userId']))
-    data = cur.fetchone()
-    cur.execute("INSERT INTO cart_items(cart_id, product_id, quantity) VALUES ({0}, {1}, {2})".format(data['cart_id'], id, 1))
-    cur.execute("UPDATE cart,product SET cart.total_price=cart.total_price+product.price WHERE product.product_id={}".format(id))
-    mysql.connection.commit()
-    cur.close()
-    flash('You have successfully added this product to your cart','success')
+def addToCart(name):
+    # cur = mysql.connection.cursor()
+    # result = cur.execute("SELECT * FROM cart where user_id = {} and state = 'ACTIVE'".format(session['userId']))
+    # if result == 0:
+    #     cur.execute("INSERT INTO cart(user_id, total_price, state) VALUES ({}, 0, 'ACTIVE')".format(session['userId']))
+    # cur.execute("SELECT * FROM cart where user_id = {} and state = 'ACTIVE'".format(session['userId']))
+    # data = cur.fetchone()
+    # cur.execute("INSERT INTO cart_items(cart_id, product_id, quantity) VALUES ({0}, {1}, {2})".format(data['cart_id'], id, 1))
+    # cur.execute("UPDATE cart,product SET cart.total_price=cart.total_price+product.price WHERE product.product_id={}".format(id))
+    # mysql.connection.commit()
+    # cur.close()
+    flash('You have successfully selected ' + name + ' profile', 'success')
     return redirect(url_for('index'))
 
 
