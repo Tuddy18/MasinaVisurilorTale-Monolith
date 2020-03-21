3895496f793bb0632ebcba18c23275058f1be244
tuddy <tuddy18_26@yahoo.com>
2020-03-21 15:05:29 +0200

    Added test login guard for index

--- app/home/home.py
+++ app/home/home.py
@@ -1,12 +1,17 @@
 from app import app
 from app.config import mysql
+from flask import request, render_template, flash, redirect, url_for, session
 from flask import render_template
 from app.cart.cart import addToCart
+from app.user.login import is_logged_in
+
 
 @app.route('/')
+@is_logged_in
 def index():
-    cur = mysql.connection.cursor()
-    result = cur.execute("SELECT * from product")
-    products = cur.fetchall()
-    print("PRODUCT = ",products)
-    return render_template('index.html', products=products)
+        cur = mysql.connection.cursor()
+        result = cur.execute("SELECT * from product")
+        products = cur.fetchall()
+        print("PRODUCT = ",products)
+        return render_template('index.html', products=products)
+
