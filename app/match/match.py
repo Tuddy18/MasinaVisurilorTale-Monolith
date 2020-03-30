83074e421714c12f17017e5a83b14f0c8dc73a1a
SinziToma <sanzi_tm@yahoo.com>
2020-03-30 23:26:42 +0300

    match db message

--- app/match/match.py
+++ app/match/match.py
@@ -1,93 +1,75 @@
 from app import app
-from flask import flash, redirect, url_for, session, render_template, request
+from flask import flash, redirect, url_for, session, render_template, jsonify, request
 from app.config import mysql
 from app.user.login import is_logged_in
 from datetime import datetime
 
 
-# @app.route('/addToCart/<path:name>')
-# @is_logged_in
-# def addToCart(name):
-# cur = mysql.connection.cursor()
-# result = cur.execute("SELECT * FROM match where user_id = {} and state = 'ACTIVE'".format(session['userId']))
-# if result == 0:
-#     cur.execute("INSERT INTO match(user_id, total_price, state) VALUES ({}, 0, 'ACTIVE')".format(session['userId']))
-# cur.execute("SELECT * FROM match where user_id = {} and state = 'ACTIVE'".format(session['userId']))
-# data = cur.fetchone()
-# cur.execute("INSERT INTO cart_items(cart_id, product_id, quantity) VALUES ({0}, {1}, {2})".format(data['cart_id'], id, 1))
-# cur.execute("UPDATE match,product SET match.total_price=match.total_price+product.price WHERE product.product_id={}".format(id))
-# mysql.connection.commit()
-# cur.close()
-# flash('You have successfully selected ' + name + ' profile', 'success')
-# return redirect(url_for('index'))
+@app.route('/match/add',  methods=['POST'])
+@is_logged_in
+def match_add():
+    cur = mysql.connection.cursor()
+
+    liked_profile_id = request.form["profile_id"]
+    liked = request.form["liked"]
+    now = datetime.now()
+    now = now.strftime('%Y:%m:%d %H:%M:%S')
+
+    cur.execute("SELECT * from Profile where AccountId = %s", str(session["accountId"]))
+    profile = cur.fetchone()
+    profile_id = str(profile["ProfileId"])
+
+    result = cur.execute("SELECT * FROM MatchedContact WHERE MatchedContact.FirstProfileId = {0} "
+                         "AND MatchedContact.SecondProfileId = {1}"
+                         .format(profile_id, liked_profile_id))
+
+    if result > 0:
+        cur.execute("UPDATE MatchedContact "
+                    "SET MatchedContact.FirstProfileLike = {0}, MatchedContact.MatchDateTime = \'{1}\' "
+                    "WHERE MatchedContact.FirstProfileId = {2} "
+                    "AND MatchedContact.SecondProfileId = {3};"
+                    .format(liked,  now, profile_id, liked_profile_id))
+    else:
+        result = cur.execute("SELECT * FROM MatchedContact WHERE FirstProfileId = {0} AND SecondProfileId = {1};"
+                             .format(liked_profile_id, profile_id))
+        if result > 0:
+            cur.execute("UPDATE MatchedContact "
+                        "SET FirstProfileLike = {0}, MatchDateTime = \'{1}\' "
+                        "WHERE FirstProfileId = {2} AND SecondProfileId = {3};"
+                        .format(liked, datetime.now(), liked_profile_id, profile_id))
+        else:
+            cur.execute("INSERT INTO MatchedContact(FirstProfileId, SecondProfileId"
+                        ", FirstProfileLike, MatchDateTime) "
+                        "VALUES({0}, {1}, {2}, \'{3}\');"
+                        .format(profile_id, liked_profile_id, liked, now))
+    mysql.connection.commit()
+    resp = jsonify(success=True)
+    return resp
 
 
-@app.route('/match', methods=['POST'])
+@app.route('/match')
 @is_logged_in
 def match():
     cur = mysql.connection.cursor()
     cur.execute("SELECT * from Profile where AccountId = %s", str(session["accountId"]))
     profile = cur.fetchone()
-    cur.execute("SELECT * FROM photo WHERE photo.ProfileId != {0} AND photo.ProfileId NOT IN "
-                "(SELECT matchedcontact.FirstProfileId "
-                "FROM matchedcontact "
-                "WHERE matchedcontact.SecondProfileId = {0} AND matchedcontact.SecondProfileLike = {0} "
-                "UNION SELECT matchedcontact.SecondProfileId "
-                "FROM matchedcontact "
-                "WHERE matchedcontact.FirstProfileId = {0} AND matchedcontact.FirstProfileLike = {0}); "
-                "LIMIT 1;"
+    cur.execute("SELECT * FROM Photo "
+                "INNER JOIN (SELECT * FROM Photo "
+                "WHERE Photo.ProfileId != {0} AND photo.ProfileId NOT IN ("
+                "SELECT matchedcontact.FirstProfileId FROM matchedcontact "
+                "WHERE matchedcontact.SecondProfileId = {0} "
+                "AND matchedcontact.SecondProfileLike != NULL "
+                "UNION "
+                "SELECT matchedcontact.SecondProfileId FROM matchedcontact "
+                "WHERE matchedcontact.FirstProfileId = {0} " 
+                "AND matchedcontact.FirstProfileLike != NULL ) LIMIT 1) t "
+                "ON Photo.ProfileId = t.ProfileId;"
                 .format(str(profile["ProfileId"])))
     recommendations = cur.fetchall()
-    if recommendations == 0:
-        flash('There are no recommendations for you at the moment')
-    else:
+    if recommendations > 0:
         return render_template('match.html', recommendations=recommendations)
-
-    liked = False
-    profile_id = str(profile["ProfileId"])
-    likedProfile_id = recommendations[0].ProfileId
-
-    if request.method == 'POST':
-        if request.form['submit_button'] == 'Like':
-            liked = True
-        elif request.form['submit_button'] == 'Dislike':
-            liked = False
-        cur.execute("SELECT * FROM MatchedContact "
-                    "WHERE MatchedContact.FirstProfileId = {0}} "
-                    "AND MatchedContact.SecondProfileId = {1}}"
-                    .format(profile_id, likedProfile_id))
-        mysql.connection.commit()
-        if cur.fetchall() != 0:
-            cur.execute("UPDATE TABLE MatchedContact "
-                        "SET MatchedContact.FirstProfileLike = {0}, MatchedContact.MatchedDateTime = {1} "
-                        "WHERE MatchedContact.FirstProfileId = {2} "
-                        "AND MatchedContact.SecondProfileId = {3};"
-                        .format(liked, datetime.now(), profile_id, likedProfile_id))
-            mysql.connection.commit()
-        else:
-            cur.execute("SELECT * FROM MatchedContact "
-                        "WHERE FirstProfileId = {0} AND SecondProfileId = {1};"
-                        .format(likedProfile_id, profile_id))
-            mysql.connection.commit()
-            if cur.fetchall() != 0:
-                cur.execute("UPDATE TABLE MatchedContact "
-                            "SET FirstProfileLike = {0}, MatchedDateTime = {1} "
-                            "WHERE FirstProfileId = {2} AND SecondProfileId = {3};"
-                            .format(liked, datetime.now(), likedProfile_id, profile_id))
-                mysql.connection.commit()
-            else:
-                cur.execute("INSERT INTO MatchedContact(FirstProfileId, SecondProfileId"
-                            ", FirstProfileLike, FirstProfileLike, MatchedDateTime) "
-                            "VALUES({0}, {1}, {2}, {3});"
-                            .format(profile_id, likedProfile_id, liked, datetime.now()))
-                mysql.connection.commit()
-
-    #cur.fetchall()
-    # recommandations = cur.fetchall()
-    # if result > 0:
-    #   return render_template('match.html', recommandations=recommandations)
-    # else:
-    #   return render_template('matchEmpty.html')
+    else:
+        flash('There are no recommendations for you at the moment')
 
     mysql.connection.commit()
     cur.close()
--- sqldump/data/db.sql
+++ sqldump/data/db.sql
@@ -53,12 +53,11 @@ CREATE TABLE IF NOT EXISTS Features (
 
 CREATE TABLE IF NOT EXISTS Message (
     MessageId INT(50) NOT NULL AUTO_INCREMENT,
-    FirstProfileId INT(50) NOT NULL,
-    SecondProfileId INT(50) NOT NULL,
+    MatchedContactId INT(50) NOT NULL,
     MessageDateTime DATETIME NOT NULL,
+    MessageText VARCHAR(500) NOT NULL,
     CONSTRAINT PK_Message PRIMARY KEY (MessageId),
-    CONSTRAINT FK_Message_First_Profile FOREIGN KEY (FirstProfileId) REFERENCES Profile(ProfileId),
-    CONSTRAINT FK_Message_Second_Profile FOREIGN KEY (SecondProfileId) REFERENCES Profile(ProfileId)
+    CONSTRAINT FK_Message_MatchedContact FOREIGN KEY (MatchedContactId) REFERENCES MatchedContact(MatchedContactId)
 );
 
 CREATE TABLE IF NOT EXISTS MatchedContact (
@@ -83,7 +82,7 @@ INSERT INTO Profile(AccountId, Name, ProfileType, Description ) VALUES(2, 'Chevy
 INSERT INTO Profile(AccountId, Name, ProfileType, Description ) VALUES(3, 'Beatle', 'Car', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas interdum venenatis bibendum. Maecenas id porta nibh. Quisque ut lorem viverra, maximus risus sit amet, aliquet nulla. Aliquam erat volutpat. Etiam ac pellentesque ante. Mauris condimentum molestie tristique. Phasellus sit amet vehicula metus.');
 INSERT INTO Profile(AccountId, Name, ProfileType, Description ) VALUES(4, 'Mercedes', 'Car', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas interdum venenatis bibendum. Maecenas id porta nibh. Quisque ut lorem viverra, maximus risus sit amet, aliquet nulla. Aliquam erat volutpat. Etiam ac pellentesque ante. Mauris condimentum molestie tristique. Phasellus sit amet vehicula metus.');
 
-INSERT INTO Photo(ProfileId, Url) VALUES(1, 'https://www.bmw-m.com/content/dam/bmw/marketBMW_M/common/topics/magazine-article-pool/2019/bmw-m-wallpaper/bmw-m850i-individual-night-sky-gallery-01.jpg')
+INSERT INTO Photo(ProfileId, Url) VALUES(1, 'https://www.bmw-m.com/content/dam/bmw/marketBMW_M/common/topics/magazine-article-pool/2019/bmw-m-wallpaper/bmw-m850i-individual-night-sky-gallery-01.jpg');
 
 INSERT INTO Photo(ProfileId, Url) VALUES(2, 'https://media.apnarm.net.au/media/images/2018/09/10/b881570332z1_20180910142638_000gnu1850e21-0-q7lilirpso9msbdgxq2_fct2072x1554x622x452_t1880.jpg');
 INSERT INTO Photo(ProfileId, Url) VALUES(2, 'https://drivetribe.imgix.net/RYeyMS-OS2-XkAX4R0grRg?w=1400&h=788&fm=webp&auto=compress&lossless=true&fit=crop&crop=faces');
@@ -96,3 +95,7 @@ INSERT INTO MatchedContact(FirstProfileId, SecondProfileId, FirstProfileLike, Ma
 VALUES (1, 3, true, '2017:10:10');
 INSERT INTO MatchedContact(FirstProfileId, SecondProfileId, SecondProfileLike, MatchDateTime)
 VALUES (4, 1, true, '2017:11:11');
+INSERT INTO MatchedContact(FirstProfileId, SecondProfileId, SecondProfileLike, MatchDateTime)
+VALUES (3, 1, true, '2017:11:11');
+
+
