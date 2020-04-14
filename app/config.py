8e118f6fa93c31e880f28e5fe4e4c7537e3fd7ac
tuddy <tuddy18_26@yahoo.com>
2020-04-14 17:35:55 +0300

    connected to remotemysql db

--- app/config.py
+++ app/config.py
@@ -2,17 +2,17 @@ from app import app
 from flask_mysqldb import MySQL
 import os
 
-# app.config['MYSQL_HOST'] = 'sql12.freemysqlhosting.net'
-# app.config['MYSQL_USER'] = 'sql12186197'
-# app.config['MYSQL_PASSWORD'] = 'v1LbHD2jDU'
-# app.config['MYSQL_DB'] = 'sql12186197'
-# app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
-
-app.config['MYSQL_HOST'] = '127.0.0.1'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'masina_visurilor_tale'
+app.config['MYSQL_HOST'] = 'remotemysql.com'
+app.config['MYSQL_USER'] = 'eYHL3f9DbJ'
+app.config['MYSQL_PASSWORD'] = 'nWp7J02COR'
+app.config['MYSQL_DB'] = 'eYHL3f9DbJ'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+# app.config['MYSQL_HOST'] = '127.0.0.1'
+# app.config['MYSQL_USER'] = 'root'
+# app.config['MYSQL_PASSWORD'] = ''
+# app.config['MYSQL_DB'] = 'masina_visurilor_tale'
+# app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 # app.config['MYSQL_HOST'] = os.environ['MYSQL_DB_HOST']
 # app.config['MYSQL_USER'] = 'root'
